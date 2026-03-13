33464a4ea0569bdd8419685bf7c3de8916387a0b
shobhitag001 <shobhitag001@gmail.com>
2026-03-13 15:02:46 +0530

    UC1

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,17 @@
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=====================================");
+        System.out.println("   Welcome to Hotel Booking System   ");
+        System.out.println("           Version 1.0               ");
+        System.out.println("=====================================");
+
+        // Inform the user that the application has started
+        System.out.println("Application started successfully!");
+
+        // Application ends
+        System.out.println("Thank you for using the Hotel Booking System.");
+    }
 }
